cb8d772aed2b311f60a99f86e75109448d06c27d
Shjung <wjdtngus9@gmail.com>
2023-01-03 20:45:21 +0900

    .

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,8 +2,13 @@
 const defultResult = 0;
 let currentResult = defultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+// currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-let calculationDescription = '('+defultResult + ' + 10) * 3 / 2 - 1'
+function add(){
+    currentResult = currentResult + userInput.value;
+    // alert('The result is' + result);
+    // return result;
+    outputResult(currentResult,'');
+}
+addBtn.addEventListener('click', add);//여기서는 함수뒤에 () 안함;
 
-outputResult(currentResult,calculationDescription);
