4178826573031af642cb2952beef965fe3a4a813
Shjung <wjdtngus9@gmail.com>
2023-01-04 22:24:34 +0900

    .

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,7 +1,7 @@
 
 const defultResult = 0;
 let currentResult = defultResult;
-
+let logEntries=[];
 // currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
@@ -13,6 +13,20 @@ function getUerNumberInput(){
     return parseInt(userInput.value);//사용자가 html에 입력한 텍스트를 정수로 바꿔 반환.
 }
 
+function writeToLog(operationIdentifier, prevResult , operationNumber , newResult){
+    const logEntry = {
+        operation: operationIdentifier,
+        prevRresult: prevResult,//계산전
+        number: operationNumber,//사용자 입력 수
+        result: newResult// 연산 결과.
+    };
+    // logEntries = [enteredNumber];// 새로운 배열을 정의할 때
+    logEntries.push(logEntry);
+    // console.log(logEntry.operation);//객체의 값을 콘솔로 얻을 수 있다.
+    console.log(logEntries);
+}
+
+
 function add(){
     const enteredNumber = getUerNumberInput();// 사용자가 입력한 정수
     const initialResult = currentResult; // 0에서 부터 시작한 계산 전의 수
@@ -20,7 +34,9 @@ function add(){
     // currentResult = currentResult + +userInput.value;//+ == perseInt
     // alert('The result is' + result);
     // return result;
-    createAndWriteOutput('+',initialResult, enteredNumber)
+    createAndWriteOutput('+',initialResult, enteredNumber);
+    writeToLog('Add',initialResult,enteredNumber,currentResult);
+    
 }
 
 function subtract(){
@@ -28,7 +44,8 @@ function subtract(){
     const initialResult = currentResult;
     currentResult -= enteredNumber;
     
-    createAndWriteOutput('-',initialResult, enteredNumber)
+    createAndWriteOutput('-',initialResult, enteredNumber);
+    writeToLog('SUBTRACT',initialResult,enteredNumber,currentResult);
 }
 
 function multiply(){
@@ -36,7 +53,8 @@ function multiply(){
     const initialResult = currentResult;
     currentResult *= enteredNumber;
     
-    createAndWriteOutput('*',initialResult, enteredNumber)
+    createAndWriteOutput('*',initialResult, enteredNumber);
+    writeToLog('multiply',initialResult,enteredNumber,currentResult);
 }
 
 function divide(){
@@ -45,6 +63,7 @@ function divide(){
     currentResult /= enteredNumber;
     
     createAndWriteOutput('/',initialResult, enteredNumber)
+    writeToLog('divide',initialResult,enteredNumber,currentResult);
 }
 addBtn.addEventListener('click', add);//여기서는 함수뒤에 () 안함;
 subtractBtn.addEventListener('click',subtract);
